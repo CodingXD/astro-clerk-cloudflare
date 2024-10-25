25e7d3c859976f2618e7382753173ea9d6387569
Marshall Shelly <Marshallshelly97@gmail.com>
2024-10-25 12:46:03 +0530

    added astro code

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import clerk from "@clerk/astro";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [clerk()],
+  output: "server",
+  adapter: cloudflare(),
+});
